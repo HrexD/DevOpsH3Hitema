f84cf07748af018a9c459f0919e121dedc0412f8
Hrex <cedgoujon78@gmail.com>
2023-04-03 00:01:48 +0200

    fix

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -3,17 +3,6 @@
 describe('Test du nom de la page d \'accueil', () => {
     it('Récupère le nom de la page d\'accueil', () => {
       cy.visit('http://localhost:5000/') 
-      cy.get('h1').should('contain', 'Don\'d dell me what do do')
+      cy.get('h1').should('contain', 'Mahamadou')
     })
-
-
   })
-  describe('Test de navigation', () => {
-    it('clique sur le bouton pour aller sur la page suivante', () => {
-      cy.visit('http://localhost:5000/');
-      
-      cy.get('#myButton').click();
-      
-      cy.url().should('include', '/champs');
-    });
-  });
